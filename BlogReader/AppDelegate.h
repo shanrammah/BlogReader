8607d90e180f12589b39d1a8afb82ce898aa0ce3
Shan Rammah <shan.rammah@gmail.com>
2016-01-06 22:34:58 -0800

    Initial Commit

---
+++ BlogReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlogReader
+//
+//  Created by Shan Rammah on 1/6/16.
+//  Copyright © 2016 Shan Rammah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlogReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlogReader
+//
+//  Created by Shan Rammah on 1/6/16.
+//  Copyright © 2016 Shan Rammah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
